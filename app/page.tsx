96fa2818aa7fd264847c7b79db39e46fdcfb6e5e
faisalsaifii <47176738+faisalsaifii@users.noreply.github.com>
2026-03-05 14:59:36 +0530

    fix: footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import AboutSection from "@/components/AboutSection";
 import ContactSection from "@/components/ContactSection";
 import EducationSection from "@/components/EducationSection";
 import ExperienceSection from "@/components/ExperienceSection";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import ProjectsSection from "@/components/ProjectsSection";
 import SkillsSection from "@/components/SkillsSection";
-import { Footer } from "react-day-picker";
 
 export default function Home() {
   return (
